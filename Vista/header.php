18f07e44b38a1acbdd580ba839caee43e79cd042
Jesus D. Alanoca <jedalcau@outlook.com>
2021-07-17 00:15:38 -0500

    add files

--- Vista/header.php
+++ Vista/header.php
@@ -4,7 +4,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=0">
     <link rel="shortcut icon" type="image/x-icon" href="assets/img/favicon.ico">
-    <title>Preclinic - Medical & Hospital - Bootstrap 4 Admin Template</title>
+    <title>Waliki |  Sistema de Registro de Historias Clinicas</title>
     <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
     <link rel="stylesheet" type="text/css" href="assets/css/bootstrap.min.css">
     <link rel="stylesheet" type="text/css" href="assets/css/font-awesome.min.css">
@@ -17,15 +17,19 @@
 <body>
 <div class="main-wrapper">
     <div class="header">
+
+        <!-- LOGO -->
         <div class="header-left">
-            <a href="index-2.html" class="logo">
-                <img src="assets/img/logo.png" width="35" height="35" alt=""> <span>Preclinic</span>
+            <a href="index.php" class="logo">
+                <img src="assets/img/logo.png" width="35" height="35" alt=""> <span>Waliki</span>
             </a>
         </div>
+
         <a id="toggle_btn" href="javascript:void(0);"><i class="fa fa-bars"></i></a>
         <a id="mobile_btn" class="mobile_btn float-left" href="#sidebar"><i class="fa fa-bars"></i></a>
         <ul class="nav user-menu float-right">
-            <li class="nav-item dropdown d-none d-sm-block">
+            <!-- Menu Notificacion -->
+            <!--<li class="nav-item dropdown d-none d-sm-block">
                 <a href="#" class="dropdown-toggle nav-link" data-toggle="dropdown"><i class="fa fa-bell-o"></i> <span class="badge badge-pill bg-danger float-right">3</span></a>
                 <div class="dropdown-menu notifications">
                     <div class="topnav-dropdown-header">
@@ -96,25 +100,31 @@
                         <a href="activities.html">View all Notifications</a>
                     </div>
                 </div>
-            </li>
-            <li class="nav-item dropdown d-none d-sm-block">
+            </li>-->
+
+            <!-- Menu Chat -->
+            <!--<li class="nav-item dropdown d-none d-sm-block">
                 <a href="javascript:void(0);" id="open_msg_box" class="hasnotifications nav-link"><i class="fa fa-comment-o"></i> <span class="badge badge-pill bg-danger float-right">8</span></a>
-            </li>
+            </li>-->
+
+            <!-- MENU USUARIO -->
             <li class="nav-item dropdown has-arrow">
                 <a href="#" class="dropdown-toggle nav-link user-link" data-toggle="dropdown">
                         <span class="user-img">
 							<img class="rounded-circle" src="assets/img/user.jpg" width="24" alt="Admin">
 							<span class="status online"></span>
 						</span>
-                    <span>Admin</span>
+                    <span>USUARIO</span>
                 </a>
                 <div class="dropdown-menu">
-                    <a class="dropdown-item" href="profile.html">My Profile</a>
-                    <a class="dropdown-item" href="edit-profile.html">Edit Profile</a>
+                    <a class="dropdown-item" href="profile.php">My Profile</a>
+                    <a class="dropdown-item" href="edit-profile.php">Edit Profile</a>
                     <a class="dropdown-item" href="settings.html">Settings</a>
                     <a class="dropdown-item" href="login.html">Logout</a>
                 </div>
             </li>
+            <!-- FIN MENU USUARIO-->
+
         </ul>
         <div class="dropdown mobile-user-menu float-right">
             <a href="#" class="dropdown-toggle" data-toggle="dropdown" aria-expanded="false"><i class="fa fa-ellipsis-v"></i></a>
@@ -132,22 +142,22 @@
                 <ul>
                     <li class="menu-title">Main</li>
                     <li class="active">
-                        <a href="index-2.html"><i class="fa fa-dashboard"></i> <span>Dashboard</span></a>
+                        <a href="index.php"><i class="fa fa-dashboard"></i> <span>Principal</span></a>
                     </li>
                     <li>
-                        <a href="doctors.html"><i class="fa fa-user-md"></i> <span>Doctors</span></a>
+                        <a href="doctores.php"><i class="fa fa-user-md"></i> <span>Doctores</span></a>
                     </li>
                     <li>
-                        <a href="patients.html"><i class="fa fa-wheelchair"></i> <span>Patients</span></a>
+                        <a href="pacientes.php"><i class="fa fa-wheelchair"></i> <span>Pacientes</span></a>
                     </li>
                     <li>
-                        <a href="appointments.html"><i class="fa fa-calendar"></i> <span>Appointments</span></a>
+                        <a href="citas.php"><i class="fa fa-calendar"></i> <span>Citas</span></a>
                     </li>
                     <li>
                         <a href="schedule.html"><i class="fa fa-calendar-check-o"></i> <span>Doctor Schedule</span></a>
                     </li>
                     <li>
-                        <a href="departments.html"><i class="fa fa-hospital-o"></i> <span>Departments</span></a>
+                        <a href="departamentos.php"><i class="fa fa-hospital-o"></i> <span>Departamentos</span></a>
                     </li>
                     <li class="submenu">
                         <a href="#"><i class="fa fa-user"></i> <span> Employees </span> <span class="menu-arrow"></span></a>
