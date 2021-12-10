676c404e72d3023973e7e33de8692c530d10b0dd
Jesus D. Alanoca <jedalcau@outlook.com>
2021-12-09 20:37:58 -0500

    index.php update

--- view/index.php
+++ view/index.php
@@ -9,22 +9,26 @@
     <link rel="stylesheet" type="text/css" href="assets/css/bootstrap.min.css">
     <link rel="stylesheet" type="text/css" href="assets/css/font-awesome.min.css">
     <link rel="stylesheet" type="text/css" href="assets/css/style.css">
-    <!--[if lt IE 9]>
-		<script src="assets/js/html5shiv.min.js"></script>
-		<script src="assets/js/respond.min.js"></script>
-	<![endif]-->
+    <!--[if lt IE 9]-->
+		<!-- <script src="../view/assets/js/h html5shiv.min.js"></script>
+		<script src="../view/assets/js/respond.min.js"></script>-->
+	<!--[endif]-->
 </head>
 
 <body>
     <div class="main-wrapper">
         <div class="header">
+            <!-- LOGO SECTION -->
 			<div class="header-left">
-				<a href="index-2.html" class="logo">
-					<img src="assets/img/logo.png" width="35" height="35" alt=""> <span>Preclinic</span>
+				<a href="" class="logo">
+					<img src="assets/img/logo.png" width="35" height="35" alt=""> <span>WALIKI</span>
 				</a>
 			</div>
+            <!-- FIN LOGO SECTION -->
+
 			<a id="toggle_btn" href="javascript:void(0);"><i class="fa fa-bars"></i></a>
             <a id="mobile_btn" class="mobile_btn float-left" href="#sidebar"><i class="fa fa-bars"></i></a>
+
             <ul class="nav user-menu float-right">
                 <li class="nav-item dropdown d-none d-sm-block">
                     <a href="#" class="dropdown-toggle nav-link" data-toggle="dropdown"><i class="fa fa-bell-o"></i> <span class="badge badge-pill bg-danger float-right">3</span></a>
@@ -117,16 +121,20 @@
 					</div>
                 </li>
             </ul>
+            <!-- USUARIO SECTION -->
             <div class="dropdown mobile-user-menu float-right">
                 <a href="#" class="dropdown-toggle" data-toggle="dropdown" aria-expanded="false"><i class="fa fa-ellipsis-v"></i></a>
                 <div class="dropdown-menu dropdown-menu-right">
-                    <a class="dropdown-item" href="profile.html">My Profile</a>
-                    <a class="dropdown-item" href="edit-profile.html">Edit Profile</a>
-                    <a class="dropdown-item" href="settings.html">Settings</a>
-                    <a class="dropdown-item" href="login.html">Logout</a>
+                    <a class="dropdown-item" href="profile.html">Mi Perfil</a>
+                    <a class="dropdown-item" href="edit-profile.html">Editar Perfil</a>
+                    <a class="dropdown-item" href="settings.html">Configuraciones</a>
+                    <a class="dropdown-item" href="login.html">Cerrar Sesion</a>
                 </div>
             </div>
+            <!-- FIN USUARIO SECTION -->
         </div>
+
+        <!-- MENU IZQUIERDO -->
         <div class="sidebar" id="sidebar">
             <div class="sidebar-inner slimscroll">
                 <div id="sidebar-menu" class="sidebar-menu">
@@ -290,6 +298,9 @@
                 </div>
             </div>
         </div>
+        <!-- FIN MENU IZQUIERDO -->
+
+        <!-- PRINCIPAL SECTION -->
         <div class="page-wrapper">
             <div class="content">
                 <div class="row">
@@ -877,6 +888,8 @@
                 </div>
             </div>
         </div>
+        <!-- FIN PRINCIPAL SECTION -->
+
     </div>
     <div class="sidebar-overlay" data-reff=""></div>
     <script src="assets/js/jquery-3.2.1.min.js"></script>
